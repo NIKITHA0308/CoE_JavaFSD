8bcf611e923b8215432591f12cba6e77c9231a94
NIKITHA0308 <124806488+NIKITHA0308@users.noreply.github.com>
2025-03-14 14:10:42 +0530

    added

---
+++ core/src/main/java/com/myTraining/core/models/HelloWorldModel.java
@@ -0,0 +1,146 @@
+///*
+// *  Copyright 2015 Adobe Systems Incorporated
+// *
+// *  Licensed under the Apache License, Version 2.0 (the "License");
+// *  you may not use this file except in compliance with the License.
+// *  You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// *  Unless required by applicable law or agreed to in writing, software
+// *  distributed under the License is distributed on an "AS IS" BASIS,
+// *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// *  See the License for the specific language governing permissions and
+// *  limitations under the License.
+// */
+//package com.myTraining.core.models;
+//
+//import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+//
+//import javax.annotation.PostConstruct;
+//
+//import com.myTraining.core.models.Impl.AuthorModelImpl;
+//import org.apache.sling.api.resource.Resource;
+//import org.apache.sling.api.resource.ResourceResolver;
+//import org.apache.sling.models.annotations.Default;
+//import org.apache.sling.models.annotations.Model;
+//import org.apache.sling.models.annotations.injectorspecific.*;
+//
+//import com.day.cq.wcm.api.Page;
+//import com.day.cq.wcm.api.PageManager;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import java.util.Optional;
+//
+//@Model(adaptables = Resource.class)
+//public class HelloWorldModel {
+//
+//    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+//    @Default(values="No resourceType")
+//    protected String resourceType;
+//    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldModel.class);
+//
+//     @Self
+//     Resource resource;
+//
+//
+//    @SlingObject
+//    private Resource currentResource;
+//
+//   // @SlingObject
+//    private ResourceResolver resourceResolver;
+//
+//    private String message;
+//
+//    @PostConstruct
+//    protected void init() {
+//
+//     resourceResolver=resource.getResourceResolver();
+//
+//
+//        LOG.info( "-----hello world  get path---"+resource.getPath());
+//
+//        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+//        String currentPagePath = Optional.ofNullable(pageManager)
+//                .map(pm -> pm.getContainingPage(currentResource))
+//                .map(Page::getPath).orElse("");
+//
+//        message = "Hello!\n"
+//            + "Resource type is: " + resourceType + "\n"
+//            + "Current page is:  " + currentPagePath + "\n";
+//    }
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//}
+
+/*
+ *  Copyright 2015 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.myTraining.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import java.util.Optional;
+
+@Model(adaptables = Resource.class)
+public class HelloWorldModel {
+
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+    @Default(values="No resourceType")
+    protected String resourceType;
+
+
+    @SlingObject
+    private Resource currentResource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private String message;
+
+    @PostConstruct
+    protected void init() {
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        String currentPagePath = Optional.ofNullable(pageManager)
+                .map(pm -> pm.getContainingPage(currentResource))
+                .map(Page::getPath).orElse("");
+
+        message = "Hello World!\n"
+            + "Resource type is: " + resourceType + "\n"
+            + "Current page is:  " + currentPagePath + "\n";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
